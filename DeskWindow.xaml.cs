3d53c6bb264c0a3eb65e100cccda63d76dcaef60
StardustSeemsInk <114210472+StardustSeemsInk@users.noreply.github.com>
2022-11-26 10:17:53 +0800

    💾 Feat: Init Kxp-DeskTopPlayet

---
+++ DeskWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace DeskTopPlayer
+{
+    /// <summary>
+    /// DeskWindow.xaml 的交互逻辑
+    /// </summary>
+
+    public partial class DeskWindow : Window
+    {
+
+        public DeskWindow()
+        {
+            InitializeComponent();
+            //this.Icon = new BitmapImage(new Uri(Environment.CurrentDirectory + @"\assets\th.ico"));
+        }
+
+        public void MediaPlay(string videoPath)
+        {
+            MediaPlayer.Source = new Uri(videoPath);
+
+            MediaPlayer.Play();
+        }
+
+        private void MediaPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer.Position -= MediaPlayer.Position;
+        }
+    }
+}
---
+++ Tools.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace DeskTopPlayer
+{
+
+    class Tools
+    {
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string className, string? winName);
+
+        [DllImport("user32.dll")]
+        public static extern bool EnumWindows(EnumWindowsProc proc, IntPtr lParam);
+        public delegate bool EnumWindowsProc(IntPtr hwnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetParent(IntPtr hwnd, IntPtr parentHwnd);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string className, string? winName);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageTimeout(IntPtr hwnd, uint msg, IntPtr wParam, IntPtr lParam, uint fuFlage, uint timeout, IntPtr result);
+
+        public static void SetDeskTop(Window wpfWindow)
+        {
+            IntPtr programIntPtr = Tools.FindWindow("Progman", null);
+            
+            Console.WriteLine(programIntPtr);
+            // 窗口句柄有效
+            if (programIntPtr != IntPtr.Zero)
+            {
+                IntPtr result = IntPtr.Zero;
+                SendMessageTimeout(programIntPtr, 0x052C, IntPtr.Zero, IntPtr.Zero, 0x0000, 1000, IntPtr.Zero);
+                // 遍历顶级窗口
+                Tools.EnumWindows((hwnd, lParam) =>
+                {
+                    // 找到包含 SHELLDLL_DefView 这个窗口句柄的 WorkerW
+                    if (Tools.FindWindowEx(hwnd, IntPtr.Zero, "SHELLDLL_DefView", null) != IntPtr.Zero)
+                    {
+                        // 找到当前 WorkerW 窗口的，后一个 WorkerW 窗口。 
+                        IntPtr tempHwnd = Tools.FindWindowEx(IntPtr.Zero, hwnd, "WorkerW", null);
+
+                        // 隐藏这个窗口
+                        Tools.ShowWindow(tempHwnd, 0);
+                    }
+                    return true;
+                }, IntPtr.Zero);
+            }
+
+            Tools.SetParent(new System.Windows.Interop.WindowInteropHelper(wpfWindow).Handle, programIntPtr);
+
+        }
+    }
+}
