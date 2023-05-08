7c38bd23c3a499d9844d620b400c4d9943592bb9
Dynesshely <catrol@qq.com>
2023-05-08 16:20:40 +0800

    🧩 🐛 Refactor, Bug(ClassScheduler.WPF): Refactored most codes and updated `KitX.Contract.CSharp`
    Bug:
    
    - Setting as desktop does not work on Windows 11.

--- ClassScheduler.WPF/DeskWindow.xaml.cs
+++ ClassScheduler.WPF/DeskWindow.xaml.cs
@@ -1,68 +1,63 @@
 ﻿using System;
+using System.Timers;
 using System.Windows;
-using System.Windows.Threading;
 
-namespace DeskTopPlayer
+namespace ClassScheduler.WPF;
+
+/// <summary>
+/// DeskWindow.xaml 的交互逻辑
+/// </summary>
+
+public partial class DeskWindow : Window
 {
-    /// <summary>
-    /// DeskWindow.xaml 的交互逻辑
-    /// </summary>
+    private Timer? timer;
 
-    public partial class DeskWindow : Window
+    public DeskWindow()
     {
-        private DispatcherTimer? timer;
+        InitializeComponent();
 
-        public DeskWindow()
-        {
-            InitializeComponent();
-            this.Loaded += new RoutedEventHandler(DTW_Loaded);
-            this.Clock.Text = DateTime.Now.ToString("t");
-        }
+        Loaded += new RoutedEventHandler(DTW_Loaded);
 
-        private void DTW_Loaded(object sender, RoutedEventArgs e)
-        {
-             //设置定时器
-            timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(600000000);   //时间间隔为60秒
-            timer.Tick += new EventHandler(timer_Tick);
-
-            //开启定时器
-            timer.Start();
-        }
-
-    public void ChangeDays(string text)
-        {
-            LeftDays.Text= text;
-        }
-        
-        public void ChangeCharts(string text)
-        {
-            int a = text.Length;
-            string retext = "";
-            for (int i = 0; i < a; i++)
-            {
-                string s = text[i] + "\n";
-                retext += s;
-            }
-            Charts.Text= retext;
-        }
-
-        public void MediaPlay(string videoPath)
-        {
-            MediaPlayer.Source = new Uri(videoPath);
-
-            MediaPlayer.Play();
-        }
-
-        private void MediaPlayer_MediaEnded(object sender, RoutedEventArgs e)
-        {
-            MediaPlayer.Position -= MediaPlayer.Position;
-        }
-
-        private void timer_Tick(object sender, EventArgs e)
-        {
-            this.Clock.Text = DateTime.Now.ToString("t");
-        }
-
+        Clock.Text = DateTime.Now.ToString("t");
     }
+
+    private void DTW_Loaded(object sender, RoutedEventArgs e)
+    {
+        //设置定时器
+        timer = new()
+        {
+            Interval = 60 * 1000,   //时间间隔为60秒
+            AutoReset = true,
+        };
+        timer.Elapsed += (_, _) => Clock.Text = DateTime.Now.ToString("t");
+
+        //开启定时器
+        timer.Start();
+    }
+
+    public void ChangeDays(string text) => LeftDays.Text = text;
+
+    public void ChangeCharts(string text)
+    {
+        var a = text.Length;
+        var retext = "";
+
+        for (var i = 0; i < a; ++i)
+        {
+            var s = $"{text[i]}\n";
+            retext += s;
+        }
+
+        Charts.Text = retext;
+    }
+
+    public void MediaPlay(string videoPath)
+    {
+        MediaPlayer.Source = new(videoPath);
+
+        MediaPlayer.Play();
+    }
+
+    private void MediaPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        => MediaPlayer.Position -= MediaPlayer.Position;
 }
--- ClassScheduler.WPF/Tools.cs
+++ ClassScheduler.WPF/Tools.cs
@@ -2,57 +2,80 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 
-namespace DeskTopPlayer
+namespace ClassScheduler.WPF;
+
+class Tools
 {
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
 
-    class Tools
+    [DllImport("user32.dll")]
+    public static extern IntPtr FindWindow(string className, string? winName);
+
+    [DllImport("user32.dll")]
+    public static extern bool EnumWindows(EnumWindowsProc proc, IntPtr lParam);
+    public delegate bool EnumWindowsProc(IntPtr hwnd, IntPtr lParam);
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr SetParent(IntPtr hwnd, IntPtr parentHwnd);
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string className, string? winName);
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr SendMessageTimeout(IntPtr hwnd, uint msg, IntPtr wParam, IntPtr lParam, uint fuFlage, uint timeout, IntPtr result);
+
+    public static void SetDeskTop(Window wpfWindow)
     {
-        [DllImport("user32.dll")]
-        public static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
-        [DllImport("user32.dll")]
-        public static extern IntPtr FindWindow(string className, string? winName);
+        var programIntPtr = FindWindow("Progman", null);
 
-        [DllImport("user32.dll")]
-        public static extern bool EnumWindows(EnumWindowsProc proc, IntPtr lParam);
-        public delegate bool EnumWindowsProc(IntPtr hwnd, IntPtr lParam);
+        Console.WriteLine(programIntPtr);
 
-        [DllImport("user32.dll")]
-        public static extern IntPtr SetParent(IntPtr hwnd, IntPtr parentHwnd);
-
-        [DllImport("user32.dll")]
-        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string className, string? winName);
-
-        [DllImport("user32.dll")]
-        public static extern IntPtr SendMessageTimeout(IntPtr hwnd, uint msg, IntPtr wParam, IntPtr lParam, uint fuFlage, uint timeout, IntPtr result);
-
-        public static void SetDeskTop(Window wpfWindow)
+        // 窗口句柄有效
+        if (programIntPtr != IntPtr.Zero)
         {
-            IntPtr programIntPtr = Tools.FindWindow("Progman", null);
-            
-            Console.WriteLine(programIntPtr);
-            // 窗口句柄有效
-            if (programIntPtr != IntPtr.Zero)
+            var result = IntPtr.Zero;
+
+            SendMessageTimeout(
+                programIntPtr,
+                0x052C,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                0x0000,
+                1000,
+                IntPtr.Zero
+            );
+
+            // 遍历顶级窗口
+            EnumWindows((hwnd, lParam) =>
             {
-                IntPtr result = IntPtr.Zero;
-                SendMessageTimeout(programIntPtr, 0x052C, IntPtr.Zero, IntPtr.Zero, 0x0000, 1000, IntPtr.Zero);
-                // 遍历顶级窗口
-                Tools.EnumWindows((hwnd, lParam) =>
+                // 找到包含 SHELLDLL_DefView 这个窗口句柄的 WorkerW
+                if (FindWindowEx(
+                        hwnd,
+                        IntPtr.Zero,
+                        "SHELLDLL_DefView",
+                        null
+                    ) != IntPtr.Zero)
                 {
-                    // 找到包含 SHELLDLL_DefView 这个窗口句柄的 WorkerW
-                    if (Tools.FindWindowEx(hwnd, IntPtr.Zero, "SHELLDLL_DefView", null) != IntPtr.Zero)
-                    {
-                        // 找到当前 WorkerW 窗口的，后一个 WorkerW 窗口。 
-                        IntPtr tempHwnd = Tools.FindWindowEx(IntPtr.Zero, hwnd, "WorkerW", null);
+                    // 找到当前 WorkerW 窗口的，后一个 WorkerW 窗口。 
+                    var tempHwnd = FindWindowEx(
+                        IntPtr.Zero,
+                        hwnd,
+                        "WorkerW",
+                        null
+                    );
 
-                        // 隐藏这个窗口
-                        Tools.ShowWindow(tempHwnd, 0);
-                    }
-                    return true;
-                }, IntPtr.Zero);
-            }
-
-            Tools.SetParent(new System.Windows.Interop.WindowInteropHelper(wpfWindow).Handle, programIntPtr);
-
+                    // 隐藏这个窗口
+                    ShowWindow(tempHwnd, 0);
+                }
+                return true;
+            }, IntPtr.Zero);
         }
+
+        SetParent(
+            new System.Windows.Interop.WindowInteropHelper(wpfWindow).Handle,
+            programIntPtr
+        );
+
     }
 }
