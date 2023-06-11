210eed88472fc151a3ae0a91b40b37f5b430c5ae
StardustSeemsInk <114210472+StardustSeemsInk@users.noreply.github.com>
2023-06-11 20:39:31 +0800

    💾 Feat: 完善Function的调用

--- ClassScheduler.WPF/Controller.cs
+++ ClassScheduler.WPF/Controller.cs
@@ -30,8 +30,90 @@ public class Controller : IController
         mainwin.WinExit();
     }
 
+    #region 声明函数
+    private Function ssv = new()
+    {
+        Name = "SetSoundVolume",
+        DisplayNames = new Dictionary<string, string>()
+            {
+                { "zh-cn", "设置音量" },
+                { "en-us", "Set Sound Volume" }
+            },
+        Parameters = new Dictionary<string, Dictionary<string, string>>()
+            {
+                {
+                    "volume",
+                    new Dictionary<string, string>()
+                    {
+                        { "zh-cn", "音量" },
+                        { "en-us", "Volume" }
+                    }
+                }
+            },
+        ParametersType = new List<string>()
+            {
+                "double"
+            },
+        HasAppendParameters = false,
+        ReturnValueType = "void"
+    };
+
+    private Function cm = new()
+    {
+        Name = "ChangeMedia",
+        DisplayNames = new Dictionary<string, string>()
+        {
+            { "zh-cn", "更换播放内容" },
+            { "en-us", "Change Media" }
+        },
+        Parameters = new Dictionary<string, Dictionary<string, string>>()
+        {
+            {
+                "path",
+                new Dictionary<string, string>()
+                {
+                    { "zh-cn", "文件路径" },
+                    { "en-us", "FilePath" }
+                }
+            }
+        },
+        ParametersType = new List<string>()
+        {
+            "string"
+        },
+        HasAppendParameters = false,
+        ReturnValueType = "void"
+    };
+
+    private Function gsv = new()
+    {
+        Name =  "GetSoundVolume",
+        DisplayNames = new Dictionary<string, string>()
+        {
+            { "zh-cn", "获取音量数据" },
+            { "en-us", "Get Sound Volume" }
+        },
+        HasAppendParameters = false,
+        ReturnValueType = "double"
+    };
+
+    private Function gmp = new()
+    {
+        Name = "GetMediaPath",
+        DisplayNames = new Dictionary<string, string>()
+        {
+            { "zh-cn", "获取当前播放的媒体文件的路径" },
+            { "en-us", "Get the Path of the Current Media File" }
+        },
+        HasAppendParameters = false,
+        ReturnValueType = "string"
+    };
+    #endregion
+
     public List<Function> GetFunctions()
     {
+
+
         return new List<Function>()
         {
             new Function()
@@ -61,90 +143,28 @@ public class Controller : IController
                 ReturnValueType = "void"
             },
 
-            new Function()
-            {
-                Name = "SetSoundVolume",
-                DisplayNames = new Dictionary<string, string>()
-                {
-                    { "zh-cn", "设置音量" },
-                    { "en-us", "Set Sound Volume" }
-                },
-                Parameters = new Dictionary<string, Dictionary<string, string>>()
-                {
-                    {
-                        "volume",
-                        new Dictionary<string, string>()
-                        {
-                            { "zh-cn", "音量" },
-                            { "en-us", "Volume" }
-                        }
-                    }
-                },
-                ParametersType = new List<string>()
-                {
-                    "double"
-                },
-                HasAppendParameters = false,
-                ReturnValueType = "void"
-            },
+            ssv,
 
-            new Function()
-            {
-                Name = "ChangeMedia",
-                DisplayNames = new Dictionary<string, string>()
-                {
-                    { "zh-cn", "更换播放内容" },
-                    { "en-us", "Change Media" }
-                },
-                Parameters = new Dictionary<string, Dictionary<string, string>>()
-                {
-                    {
-                        "path",
-                        new Dictionary<string, string>()
-                        {
-                            { "zh-cn", "文件路径" },
-                            { "en-us", "FilePath" }
-                        }
-                    }
-                },
-                ParametersType = new List<string>()
-                {
-                    "string"
-                },
-                HasAppendParameters = false,
-                ReturnValueType = "void"
-            },
+            cm,
 
-            new Function()
-            {
-                Name =  "GetSoundVolume",
-                DisplayNames = new Dictionary<string, string>()
-                {
-                    { "zh-cn", "获取音量数据" },
-                    { "en-us", "Get Sound Volume" }
-                },
-                HasAppendParameters = false,
-                ReturnValueType = "double"
-            },
+            gsv,
 
-            new Function()
-            {
-                Name = "GetMediaPath",
-                DisplayNames = new Dictionary<string, string>()
-                {
-                    { "zh-cn", "获取当前播放的媒体文件的路径" },
-                    { "en-us", "Get the Path of the Current Media File" }
-                },
-                HasAppendParameters = false,
-                ReturnValueType = "string"
-            },
+            gmp
 
         };
     }
 
+    private void AnalyzeCmd(Command cmd, Function kxpfunc, Action action)
+    {
+        if (cmd.Request == kxpfunc.Name) action.Invoke();
+    }
+
     public void Execute(Command cmd)
     {
-
+        AnalyzeCmd(cmd, ssv, delegate () { mainwin.KXPChangeVolume(Convert.ToDouble(cmd.RequestArgs[0])); }) ;
+        AnalyzeCmd(cmd, cm, delegate () { mainwin.KXPChangeMedia(cmd.RequestArgs[0]); }) ;
+        AnalyzeCmd(cmd, gsv, delegate () { mainwin.KXPGetSoundVolume(); }) ;
+        AnalyzeCmd(cmd, gmp, delegate () { mainwin.KXPGetMediaPath(); }) ;
     }
 
     public void SetRootPath(string path)
