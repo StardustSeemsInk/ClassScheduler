ffbcbe6f7409739d0e3792656a7e90f2c116fdc7
Dynesshely <catrol@qq.com>
2023-09-09 20:34:20 +0800

    💾 Feat: Init data and models.

---
+++ ClassScheduler.WPF/Data/Classes.cs
@@ -0,0 +1,46 @@
+﻿using ClassScheduler.WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace ClassScheduler.WPF.Data;
+
+public class Classes
+{
+    public string? Name {  get; set; }
+
+    public List<ClassModel> ClassesList { get; set; } = new();
+}
+
+public static class ClassesExtensions
+{
+    public static void Save(
+        this Classes classes,
+        string path,
+        Action<JsonSerializerOptions>? optionsProcessor = null)
+    {
+        var options = new JsonSerializerOptions()
+        {
+            IncludeFields = true,
+            WriteIndented = true,
+        };
+        optionsProcessor?.Invoke(options);
+
+        var json = JsonSerializer.Serialize(classes);
+        File.WriteAllText(path, json);
+    }
+
+    public static Classes? Load(
+        this string path,
+        Action<JsonSerializerOptions>? optionsProcessor = null)
+    {
+        var options = new JsonSerializerOptions();
+        optionsProcessor?.Invoke(options);
+
+        var json = File.ReadAllText(path);
+        var classes = JsonSerializer.Deserialize<Classes>(json);
+
+        return classes;
+    }
+}
