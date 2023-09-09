1af535315955d2b0322879a0f74ae1934297bb77
Dynesshely <catrol@qq.com>
2023-09-09 21:43:16 +0800

    💾 Feat: Organized.

---
+++ ClassScheduler.WPF/Instances.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassScheduler.WPF;
+
+internal static class Instances
+{
+    internal static void Init()
+    {
+        MainWindow = new();
+
+        Controller = new(MainWindow);
+    }
+
+    internal static MainWindow? MainWindow { get; set; }
+
+    internal static Controller? Controller { get; set; }
+}
